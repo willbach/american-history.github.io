7cea4dd3291a38b5bd0242cdb7b8d73c6757552c
Will Galebach <willbach@gmail.com>
2018-07-25 16:34:14 +0000

    Updated push script

--- push-changes.sh
+++ push-changes.sh
@@ -1,8 +1,11 @@
-echo $1
+#!/bin/bash
 
 if [ $# -eq 0 ]
   then
     echo "Please supply a commit message"
+    exit 0
 fi
+
+echo "Commit Message: $1"
 
 git pull upstream master && git add . && git commit -m "$1" && git push upstream master && git push origin master
