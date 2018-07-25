d6ff6cd7501c89b307f03976935339e80bd37651
Will Galebach <willbach@gmail.com>
2018-07-25 16:35:34 +0000

    Hardcoded upstream repo

--- pull-and-build.sh
+++ pull-and-build.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 git pull git@github.com:american-history/american-history.github.io.git master && npm start
--- push-changes.sh
+++ push-changes.sh
@@ -8,4 +8,4 @@ fi
 
 echo "Commit Message: $1"
 
-git pull upstream master && git add . && git commit -m "$1" && git push upstream master && git push origin master
+git pull upstream master && git add . && git commit -m "$1" && git push git@github.com:american-history/american-history.github.io.git master && git push origin master
